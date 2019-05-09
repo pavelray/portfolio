a33fdafa93e219cde5f21fe1a742c937951dc15f
Pavel Ray <pavelray@gmail.com>
2019-05-09 09:16:16 +0530

    Added Skills

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
+  name = 'Pavel Ray';
+  role = 'Software Engineer';
+  email = 'pavelray@gmail.com';
+  contact = '8660677881';
+  address = 'Bangalore, India';
+  aboutMe = 'Hello, I’m Pavel, web-developer based on Bangalore.' 
+              +'I have rich experience in web site design & building and customization';
+  skills = ['.NET','C#','Angular','JavaScript','HTML','CSS','SQL']
+
   constructor() { }
 
   ngOnInit() {
